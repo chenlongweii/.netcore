a4d074edd1d25625b86412e0405b31a6cba2df20
longwei <longwei.shen@baisonmail.com>
2019-05-13 09:35:56 +0800

    修改

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace myApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new WebHostBuilder()
+                .UseUrls("http://local:8564", "http://local:8565")
+                .UseKestrel()
+                
+                //.Configure(app => app.Run(async context => await context.Response.WriteAsync("Hello World")))
+                .UseStartup<Startup>()
+              .Build()
+              .Run();
+        }
+    }
+    public class HelloController : Controller
+    {
+        [HttpGet("/hello/{name}")]
+        public IActionResult SayHello(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection svcs)
+        {
+            svcs.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
         }
     }
 }
